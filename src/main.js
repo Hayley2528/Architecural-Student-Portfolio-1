823c34009c15e193eb63792e930dfe5bde6a8a30
hayley2528 <hayley.miller1@ucalgary.ca>
2025-01-19 16:29:46 -0700

    Updated portfolio

--- src/main.js
+++ src/main.js
@@ -34,7 +34,8 @@ document.querySelector("#app").innerHTML = `
         </div>
         <div id="images-description">
           <h4 id="description"> In my approach to creating this work, I took into consideration my own worldview, and how the things that I observe around me are impacting the planet. I chose to approach three distinct and yet interconnected topics that interest me; food/resource security for the future, housing/how people live, and environmental tourism/preservation. I looked at each of these topics and further specified them to my own observations and experiences. It is easy to look at the state of the world right now and feel despair around the large and seemingly hopeless issues that we face. Looking locally at the Calgary area and the province of Alberta will help us to critically analyze our own impact, and place within global systems, allowing us to take appropriate steps towards a more sustainable future. I believe that we stand at a pivotal moment in history, and that the choices we make today will shape the world for generations to come. The future is no-doubt urban, although a large part of environmental stewardship extends beyond city limits. Through this work I aim to make connections between rural and urban contexts, placing emphasis on the ways we are all connected.   </h4>
-          
+       
+          <div id="images">
 <a href="./myprojectdrawings/313 project one 1.png" target="_blank"><img src="./myprojectdrawings/313 project one 1.png"></a>
 
 <a href="./myprojectdrawings/313 project one 2.png" target="_blank"><img src="./myprojectdrawings/313 project one 2.png"></a>
@@ -48,6 +49,7 @@ document.querySelector("#app").innerHTML = `
 <a href="./myprojectdrawings/313 project one 6.png" target="_blank"><img src="./myprojectdrawings/313 project one 6.png"></a>
 
 <a href="./myprojectdrawings/313 project one 7.png" target="_blank"><img src="./myprojectdrawings/313 project one 7.png"></a>
+</div>
         </div>
       </div>
 
