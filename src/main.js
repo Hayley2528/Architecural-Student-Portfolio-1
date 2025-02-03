54eb74d6d946571573f96635113788004e79c753
hayley2528 <hayley.miller1@ucalgary.ca>
2025-02-02 22:10:59 -0700

    Updated portfolio

--- src/main.js
+++ src/main.js
@@ -36,7 +36,7 @@ document.querySelector("#app").innerHTML = `
           <h4 id="description"> In my approach to creating this work, I took into consideration my own worldview, and how the things that I observe around me are impacting the planet. I chose to approach three distinct and yet interconnected topics that interest me; food/resource security for the future, housing/how people live, and environmental tourism/preservation. I looked at each of these topics and further specified them to my own observations and experiences. It is easy to look at the state of the world right now and feel despair around the large and seemingly hopeless issues that we face. Looking locally at the Calgary area and the province of Alberta will help us to critically analyze our own impact, and place within global systems, allowing us to take appropriate steps towards a more sustainable future. I believe that we stand at a pivotal moment in history, and that the choices we make today will shape the world for generations to come. The future is no-doubt urban, although a large part of environmental stewardship extends beyond city limits. Through this work I aim to make connections between rural and urban contexts, placing emphasis on the ways we are all connected.   </h4>
        
           <div id="images">
-                    <a href="./myprojectdrawings/Corpse assignment good copy.png" target="_blank"><img src="./myprojectdrawings/Corpse assignment good copy.png"></a>
+          <a href="./myprojectdrawings/Corpse assignment good copy.png" target="_blank"><img src="./myprojectdrawings/Corpse assignment good copy.png"></a>
           <a href="./myprojectdrawings/313 project one 1.png" target="_blank"><img src="./myprojectdrawings/313 project one 1.png"></a>
           <a href="./myprojectdrawings/313 project one 2.png" target="_blank"><img src="./myprojectdrawings/313 project one 2.png"></a>
           <a href="./myprojectdrawings/313 project one 3.png" target="_blank"><img src="./myprojectdrawings/313 project one 3.png"></a>
@@ -46,8 +46,42 @@ document.querySelector("#app").innerHTML = `
           <a href="./myprojectdrawings/313 project one 7.png" target="_blank"><img src="./myprojectdrawings/313 project one 7.png"></a></div>
         </div>
       </div>
+    </section>
 
-      
+    <section id="assignment-2">
+      <div>
+        <h2>Assignment Two</h2>
+        <p>name</p>
+      </div>
+
+      <div class="flex-container">
+        <div class="box">
+          <div id="model1"></div>
+          <div>
+            <img class="image" src="/cubeDrawings/cars.jpg" alt="Car Image">
+          </div>
+        </div>
+        <div class="box">
+          <div id="model2"></div>
+          <div>
+            <img class="image" src="/cubeDrawings/cars.jpg" alt="Car Image">
+          </div>
+        </div>
+        <div class="box">
+          <div id="model3"></div>
+          <div>
+            <img class="image" src="/cubeDrawings/cars.jpg" alt="Car Image">
+          </div>
+        </div>
+      </div>
+
+      <p class="centered-text">Centered Reflection</p>
+
+      <div class="image-container">
+        <img class="image-large" src="/cubeDrawings/cube3.webp" alt="Image 1">
+        <img class="image-large" src="/cubeDrawings/cube3.webp" alt="Image 2">
+      </div>
+    </section>
       
     <ul id="footer-items">
       <li>Email: ${data.contact.email}</li>
@@ -61,6 +95,6 @@ document.querySelector("#app").innerHTML = `
 `;
 
 // Create three.js scenes for each
-createThreeScene("#model1", "/Corpse assignment jan 17 version.png");
-createThreeScene("#model2", "/Corpse assignment jan 17 version.png");
-createThreeScene("#model3", "/Corpse assignment jan 17 version.png");
+createThreeScene("#model1", "/3DModels/cube.obj");
+createThreeScene("#model2", "/3DModels/cube.obj");
+createThreeScene("#model3", "/3DModels/cube.obj");
